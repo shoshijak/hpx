9583d706e05ed4c5dce9acd61ad455bed413906b
shoshijak <as.jak@protonmail.ch>
2017-05-08 10:51:53 +0200

    draft of structure of the resourcrce_partitioner class

--- hpx/resource_partitioner/resource_partitioner.h
+++ hpx/resource_partitioner/resource_partitioner.h
@@ -8,23 +8,73 @@
 
 #include <hpx/include/runtime.hpp>
 #include <hpx/runtime/threads/topology.hpp>
+#include <hpx/runtime/threads/detail/thread_pool.hpp>
 
 #include <vector>
+#include <string>
 
 namespace hpx{
 
+    class initial_thread_pool;
+
     class resource_partitioner{
     public:
+        resource_partitioner() // queries hwloc, sets internal parameters
+                :
+                {
+                        //! do not allow the creation of more than one RP instance
+                        //! cf counter for runtime, do the same and add test and exception here
+                }
+
+        // create a new thread_pool, add it to the RP and return a pointer to it
+        initial_thread_pool* create_thread_pool(std::string name){
+            // well ... doesn't have to do that much,
+            // just push_back to the internal parameter probably
+            initial_thread_pool_.push_back(initial_thread_pool_(name));
+        }
+
+        // lots of get_functions
+        std::size_t get_number_pools(){
+            return thread_pools_.size();
+        }
 
     private:
+        // contains the basic characteristics
+        std::vector<initial_thread_pool> initial_thread_pool_;
+
+        // actual thread pools of OS-threads
+        std::vector<threads::detail::thread_pool> thread_pools_; //! template param?
+
+        // counter for instance numbers
 
     };
 
-    resource_partitioner * get_resource_partitioner_ptr()
+    resource_partitioner * get_resource_partitioner_ptr() const
     {
+        // if resource_partitioner has been instanciated already (most cases)
         resource_partitioner** rp = runtime::runtime_.get();
-        return rt ? *rt : nullptr;
+        return rp ? *rp : nullptr;
     }
+
+    // structure used to encapsulate all characteristics of thread_pools
+    // as specified by the user in int main()
+    struct initial_thread_pool{
+    public:
+        initial_thread_pool(std::string name)
+                :pool_name_(name)
+                {}
+
+        // mechanism for adding resources
+        void add_resource(std::size_t){
+            //! ...
+        }
+
+    private:
+        std::string pool_name_;
+
+    };
+
+
 
 }
 
