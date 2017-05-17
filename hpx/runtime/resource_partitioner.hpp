78de36fed91b7b94419e0cf8fb39041236fadee4
shoshijak <as.jak@protonmail.ch>
2017-05-17 15:10:26 +0200

    cleanup and commentaries

--- hpx/runtime/resource_partitioner.hpp
+++ hpx/runtime/resource_partitioner.hpp
@@ -6,18 +6,13 @@
 #if !defined(HPX_RESOURCE_PARTITIONER)
 #define HPX_RESOURCE_PARTITIONER
 
-#include <hpx/runtime/threads/topology.hpp>
-#include <hpx/runtime/threads/policies/topology.hpp>
-#include <hpx/runtime/threads/policies/hwloc_topology_info.hpp>
-#include <hpx/util/thread_specific_ptr.hpp>
 #include <hpx/runtime/threads/coroutines/detail/coroutine_self.hpp>
+#include <hpx/runtime/threads/policies/hwloc_topology_info.hpp>
+#include <hpx/runtime/threads/policies/topology.hpp>
 #include <hpx/runtime/threads/threadmanager.hpp>
+#include <hpx/runtime/threads/topology.hpp>
 #include <hpx/util/command_line_handling.hpp>
-//#include <hpx/runtime/get_resource_partitioner.hpp>
-
-
-//#include <hpx/runtime/threads/detail/thread_pool.hpp>
-//#include <hpx/include/runtime.hpp>
+#include <hpx/util/thread_specific_ptr.hpp>
 
 #include <boost/atomic.hpp>
 
@@ -27,10 +22,6 @@
 #include <algorithm>
 
 namespace hpx {
-/*
-    struct threads::policies::scheduler_base;
-    struct threads::policies::detail::affinity_data;
-    struct threads::policies::init_affinity_data;*/
 
     namespace resource {
 
@@ -57,9 +48,6 @@ namespace hpx {
 
         init_pool_data(std::string name, scheduling_policy = scheduling_policy::unspecified);
 
-        //! another constructor with size param in case the user already knows at cstrction how many resources will be allocated?
-        //! this constructor would call "reserve" on data member and be a little more mem-efficient
-
         // set functions
         void set_scheduler(scheduling_policy sched);
 
@@ -78,9 +66,6 @@ namespace hpx {
         std::string pool_name_;
         scheduling_policy scheduling_policy_;
         std::vector<std::size_t> my_pus_;
-        //! does it need to hold the information "run HPX on me/not"? ie "can be used for runtime"/not?
-        //! would make life easier for ppl who want to run HPX side-by-sie with OpenMP for example?
-
     };
 
     class HPX_EXPORT resource_partitioner{
@@ -95,7 +80,6 @@ namespace hpx {
         resource_partitioner();
 
         void print_me(); //! used by shoshijak for testing. To be deleted.
-
 
         // create a new thread_pool, add it to the RP and return a pointer to it
         void create_thread_pool(std::string name, scheduling_policy sched = scheduling_policy::unspecified);
@@ -113,13 +97,13 @@ namespace hpx {
         //! stuff that has to be done during hpx_init ...
         void set_scheduler(scheduling_policy sched, std::string pool_name);
         void set_threadmanager(threads::threadmanager_base* thrd_manag);
-//        threads::threadmanager_base* set_and_get_thread_manager(
-//                util::io_service_pool& timer_pool, threads::policies::callback_notifier& notifier);
         threads::threadmanager_base* get_thread_manager();
+
         //! called in hpx_init
         void set_affinity_data(std::size_t num_threads) {
             affinity_data_.set_num_threads(num_threads);
         }
+
         //! called by constructor of scheduler_base
         threads::policies::detail::affinity_data* get_affinity_data(){
             return &affinity_data_;
@@ -139,32 +123,19 @@ namespace hpx {
 
         // called in hpx_init
         void init_rp();
+
+        // called in runtime::assign_cores()
         size_t init(threads::policies::init_affinity_data data){
             std::size_t ret = affinity_data_.init(data, topology_);
             thread_manager_->init(data);
             return ret;
         }
 
-        // lots of get_functions
-
-/*        std::size_t get_number_pools(){
-            return thread_pools_.size();
-            // get pointer to a particular pool?
-        }*/
-
-        ////////////////////////////////////////////////////////////////////////
-
-        // this function is called in the constructor of thread_pool
-        // returns a scheduler (moved) that thread pool should have as a data member
-        hpx::threads::policies::scheduler_base* create_scheduler(std::string pool_name) const;
-
-
         ////////////////////////////////////////////////////////////////////////
 
         scheduling_policy which_scheduler(std::string pool_name);
         threads::topology& get_topology() const;
         threads::policies::init_affinity_data get_init_affinity_data() const;
-
 
 
     private:
@@ -188,23 +159,18 @@ namespace hpx {
         // that will be passed to the runtime
         std::vector<init_pool_data> initial_thread_pools_;
 
-        // reference to the threadmanager instance
+        // pointer to the threadmanager instance
         hpx::threads::threadmanager_base* thread_manager_;
 
         // reference to the topology
         threads::hwloc_topology_info& topology_;
 
         // reference to affinity data
-        //! I'll probably have to take this away from runtime
         hpx::threads::policies::init_affinity_data init_affinity_data_;
         hpx::threads::policies::detail::affinity_data affinity_data_;
 
         //! used for handing the thread_pool its scheduler. will have to be written better in the future
         hpx::util::command_line_handling* cfg_;
-
-        //! copied from thread_pool,I'm not sure whether we need this ... or whether it should go somewhere else
-        std::size_t cores_used;
-
 
     };
 
@@ -214,7 +180,6 @@ namespace hpx {
     {
         util::static_<resource::resource_partitioner, std::false_type> rp;
         return rp.get();
-        //return resource::resource_partitioner::get_resource_partitioner_();
     }
 
 
