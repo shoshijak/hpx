ef807e5257544037ae896594c1c9b0830ede3683
shoshijak <as.jak@protonmail.ch>
2017-06-02 17:06:25 +0200

    various fixes and allow RP to parse custom cmd line options

--- src/runtime.cpp
+++ src/runtime.cpp
@@ -583,7 +583,7 @@ namespace hpx
         }
 
         return static_cast<std::uint32_t>(
-            hpx::get_resource_partitioner().init(affinity_init_));
+            hpx::get_resource_partitioner().init());
     }
 
     ///////////////////////////////////////////////////////////////////////////
