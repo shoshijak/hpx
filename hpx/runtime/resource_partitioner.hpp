50f24972c28de0273f67d64ecdf8a1180773d04b
shoshijak <as.jak@protonmail.ch>
2017-06-07 09:12:59 +0200

    Remove class "init_affinity_data" as it has no use any more
    - change some functions accordingly
    - review the signature of some functions that take topology as a
    parameter

--- hpx/runtime/resource_partitioner.hpp
+++ hpx/runtime/resource_partitioner.hpp
@@ -211,7 +211,6 @@ namespace resource
         threads::topology& get_topology() const;
         util::command_line_handling& get_command_line_switches();
         std::size_t get_num_threads() const;
-        threads::policies::init_affinity_data const& get_init_affinity_data() const;
         size_t get_num_pools() const;
         size_t get_num_threads(const std::string &pool_name);
         init_pool_data* get_pool(std::size_t pool_index);
@@ -270,7 +269,6 @@ namespace resource
 
         // reference to the topology and affinity data
         threads::hwloc_topology_info& topology_;
-        hpx::threads::policies::init_affinity_data init_affinity_data_;
         hpx::threads::policies::detail::affinity_data affinity_data_;
 
         // flag set by add_resource
